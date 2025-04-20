c4a655db447c883bbf9eceb1a00b6e46761fd2b9
SIER101 <73215103+SIER101@users.noreply.github.com>
2025-04-20 23:27:00 +0700

    update1

--- src/main.js
+++ src/main.js
@@ -1,38 +1,61 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(width, height);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const colors = [0xff6f61, 0x6fa8dc, 0x93c47d];
-const materials = [
-  new THREE.MeshBasicMaterial({ color: colors[0] }), // Front
-  new THREE.MeshBasicMaterial({ color: colors[0] }), // Back
-  new THREE.MeshBasicMaterial({ color: colors[1] }), // Left
-  new THREE.MeshBasicMaterial({ color: colors[1] }), // Right
-  new THREE.MeshBasicMaterial({ color: colors[2] }), // Top
-  new THREE.MeshBasicMaterial({ color: colors[2] }), // Bottom
-];
-const cube = new THREE.Mesh(geometry, materials);
-scene.add(cube);
+const scene = new THREE.Scene();
+
+const camera = new THREE.OrthographicCamera(
+  width / -2,
+  width / 2,
+  height / 2,
+  height / -2,
+  1,
+  5000
+);
+camera.position.z = 1000;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-camera.position.z = 5;
-controls.update();
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(500, 500),
+  new THREE.MeshBasicMaterial({
+    color: 0xff6f61,
+  })
+);
+scene.add(plane);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(200, 200, 200),
+  new THREE.MeshBasicMaterial({ color: 0x93c47d })
+);
+cube2.position.set(100, 100, 100);
+scene.add(cube2);
 
 function animate() {
-  cube.rotation.y += 0.01;
-
-  // for the damping
   controls.update();
 
   renderer.render(scene, camera);
 }
+
+function onWindowResize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  camera.left = width / -2;
+  camera.right = width / 2;
+  camera.top = height / 2;
+  camera.bottom = height / -2;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(width, height);
+}
+
+window.addEventListener("resize", onWindowResize);
